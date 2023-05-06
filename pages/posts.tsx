b3a45880170694fd741755e3c71ad3a05b5866eb
seiya_tagami <bepozurapu274@gmail.com>
2023-05-06 20:52:39 +0900

    fix: add meta info

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 import PostsContainer from 'components/Posts/posts.container'
 import Footer from 'components/common/Footer'
 import Header from 'components/common/Header'
+import Meta from 'components/common/Meta'
 
 const Works = () => {
   return (
     <>
+      <Meta title='Posts' />
       <Header />
       <motion.div
         initial={{ opacity: 0 }}
--- pages/works.tsx
+++ pages/works.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 import WorksContainer from 'components/Works/works.container'
 import Footer from 'components/common/Footer'
 import Header from 'components/common/Header'
+import Meta from 'components/common/Meta'
 
 const Works = () => {
   return (
     <>
+      <Meta title='Works' />
       <Header />
       <motion.div
         initial={{ opacity: 0 }}
